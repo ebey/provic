c57519dfbc6f200d902bc0b8a8afd8c2e8efbf4f
Beylerian <ebeylerian@path.org>
2015-12-14 17:36:14 -0800

    edit colors, add plot for number of art data points

--- explore_data.R
+++ explore_data.R
@@ -35,9 +35,9 @@ barplot(rbind(tabulate(datefactor[hasARVinfo]),
         xlab = "year-month", ylab = "Number of Touchpoints",
         names.arg = plotnames[1:57],
         main = "All Touchpoints in Dataset",
-        col = c("dodgerblue", "gray"))
+        col = c("#005D6E", "gray"))
 legend("topright", legend = c("No ART info recorded", "At least some ART info"),
-       fill = c("gray", "dodgerblue"))
+       fill = c("gray", "#005D6E"))
 
 
 # totals_arvtype <- rbind(tabulate(datefactor[rawdata$ARV.Type != ""]),
@@ -61,9 +61,9 @@ barplot(rbind(tabulate(datefactor[hasARVinfo_k]),
         xlab = "year-month", ylab = "Number of Touchpoints",
         names.arg = plotnames[1:57],
         main = "Kinshasa Touchpoints Only",
-        col = c("dodgerblue", "gray"))
+        col = c("#005D6E", "gray"))
 legend("topright", legend = c("No ART info recorded", "At least some ART info"),
-       fill = c("gray", "dodgerblue"))
+       fill = c("gray", "#005D6E"))
 
 # totals_arvtype_k <- rbind(tabulate(datefactor[(rawdata$ARV.Type != "") &
 #                           (rawdata$Beneficiary.Region.Province == "Kinshasa")]),
@@ -90,9 +90,9 @@ barplot(rbind(tabulate(datefactor[hasARVinfo_kat]),
         xlab = "year-month", ylab = "Number of Touchpoints",
         names.arg = plotnames[1:57],
         main = "Katanga Touchpoints Only",
-        col = c("dodgerblue", "gray"))
+        col = c("#005D6E", "gray"))
 legend("topright", legend = c("No ART info recorded", "At least some ART info"),
-       fill = c("gray", "dodgerblue"))
+       fill = c("gray", "#005D6E"))
 
 
 # observations without a start date
@@ -100,29 +100,83 @@ legend("topright", legend = c("No ART info recorded", "At least some ART info"),
 nostart_someARV <- which(rawdata$Beginning.Date.of.Treatment == "" &
                            (rawdata$ARV.Type != "" | rawdata$ART.Start != "" |
                               rawdata$ART.Situation != "" | rawdata$ARV.Received. != "" |
-                              rawdata$ARV.Prophylaxis.Received. != ""))
+                              rawdata$ARV.Prophylaxis.Received. != "" |
+                              rawdata$ARV.TAR.Received. != ""))
 nostart_noARV <- which(rawdata$Beginning.Date.of.Treatment == "" &
                            (rawdata$ARV.Type == "" & rawdata$ART.Start == "" &
                               rawdata$ART.Situation == "" & rawdata$ARV.Received. == "" &
-                              rawdata$ARV.Prophylaxis.Received. == ""))
+                              rawdata$ARV.Prophylaxis.Received. == "" &
+                              rawdata$ARV.TAR.Received. == ""))
 
 barplot(rbind(tabulate(datefactor[nostart_someARV]),
               tabulate(datefactor[nostart_noARV])),
         xlab = "year-month", ylab = "Number of Touchpoints",
         names.arg = plotnames[1:57],
         main = "All Touchpoints without 'Beginning Date of Treatment'",
-        col = c("dodgerblue", "gray"))
+        col = c("#005D6E", "gray"))
 legend("topright", legend = c("No ART info recorded", "At least some ART info"),
-       fill = c("gray", "dodgerblue"))
+       fill = c("gray", "#005D6E"))
 
 
 # observations with a start date
-# 0 obs have a start date and no arv info
+# 216325 obs have a start date and no arv info
 withstart_someARV <- which(rawdata$Beginning.Date.of.Treatment != "" &
                            (rawdata$ARV.Type != "" | rawdata$ART.Start != "" |
                               rawdata$ART.Situation != "" | rawdata$ARV.Received. != "" |
-                              rawdata$ARV.Prophylaxis.Received. != ""))
+                              rawdata$ARV.Prophylaxis.Received. != "" |
+                              rawdata$ARV.TAR.Received. != ""))
 withstart_noARV <- which(rawdata$Beginning.Date.of.Treatment != "" &
-                         (rawdata$ARV.Type == "" & rawdata$ART.Start != "" &
-                            rawdata$ART.Situation != "" & rawdata$ARV.Received. != "" &
-                            rawdata$ARV.Prophylaxis.Received. != ""))
+                         (rawdata$ARV.Type == "" & rawdata$ART.Start == "" &
+                            rawdata$ART.Situation == "" & rawdata$ARV.Received. == "" &
+                            rawdata$ARV.Prophylaxis.Received. == "" &
+                            rawdata$ARV.TAR.Received. == ""))
+
+barplot(rbind(tabulate(datefactor[withstart_someARV]),
+              tabulate(datefactor[withstart_noARV])),
+        xlab = "year-month", ylab = "Number of Touchpoints",
+        names.arg = plotnames[1:57],
+        main = "All Touchpoints with 'Beginning Date of Treatment'",
+        col = c("#005D6E", "gray"))
+legend("topright", legend = c("No ART info recorded", "At least some ART info"),
+       fill = c("gray", "#005D6E"))
+
+# of those with ART info, what kind of info do we have?
+arvtype <- which(rawdata$ARV.Type != "")
+artstart <- which(rawdata$ART.Start != "")
+artsit <- which(rawdata$ART.Situation != "")
+arvrec <- which(rawdata$ARV.Received. != "")
+arvtarrec <- which(rawdata$ARV.TAR.Received. != "")
+arvprrec <- which(rawdata$ARV.Prophylaxis.Received. != "")
+
+barplot(rbind(tabulate(datefactor[arvtype]),
+              tabulate(datefactor[artstart]),
+              tabulate(datefactor[artsit]),
+              tabulate(datefactor[arvrec]),
+              tabulate(datefactor[arvtarrec]),
+              tabulate(datefactor[arvprrec])),
+        xlab = "year-month", ylab = "Number of Touchpoints",
+        names.arg = plotnames[1:57],
+        main = "Types of ART Information Recorded",
+        col = c("#9A3E25", "#B37055", "#708259",
+                "#95A17E", "#005D6E", "#6BBBA1"))
+legend("topleft", legend = c("ARV Type", "ART Start", "ART Situation",
+                             "ARV Received?", "ARV/TAR Received?",
+                             "ARV Prophylaxis Received?"),
+       fill = c("#9A3E25", "#B37055", "#708259",
+                "#95A17E", "#005D6E", "#6BBBA1"))
+
+
+artvars <- c("ARV.Type", "ART.Start", "ART.Situation",
+             "ARV.Received.", "ARV.TAR.Received.",
+             "ARV.Prophylaxis.Received.")
+ndatapoints <- c()
+for(i in hasARVinfo){
+  n <- 0
+  for(av in artvars){
+    if(rawdata[i,][[av]] != "") n <- n + 1
+  }
+  ndatapoints <- append(ndatapoints, n)
+}
+barplot(table(ndatapoints), main = "ART Data Points per Touchpoint (out of six)",
+        ylab = "Number of touchpoints",
+        col = "#005D6E")
