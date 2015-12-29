d6c6aaa8f4819ed18d272997d07e4c4a6ffd27a3
Emily Beylerian <ebeylerian@path.org>
2015-12-29 11:49:00 -0800

    add group size to legend and missing number to subtitle

--- analysis_funcs.R
+++ analysis_funcs.R
@@ -4,8 +4,10 @@
 survival.univ <- function(survobj, vars, covardf, subtitle = ""){
 
   for(av in vars){
-    lvls <- unique(covardf[[av]])
+    lvls <- sort(unique(covardf[[av]]))
+    mis <- sum(lvls == "", na.rm = TRUE) + sum(is.na(lvls))
     lvls <- lvls[lvls != ""]
+
     colpal <- suppressWarnings(brewer.pal(name = "YlGnBu",
                                           n = length(lvls) + 1)[-1])
     covardf[["thisvar"]] <- covardf[[av]]
@@ -13,8 +15,14 @@ survival.univ <- function(survobj, vars, covardf, subtitle = ""){
     datavals <- data.frame(thisvar = lvls)
     plot(survfit(fit, newdata = datavals), col = colpal,
          main = av, xlab = "Days in ART program",
-         ylab = "Proportion retained in treatment")
-    legend("topright", legend = lvls, fill = colpal)
+         ylab = "Proportion retained in treatment",
+         sub = paste("Missing:", mis))
+    lvlsn <- c()
+    for(lv in lvls){
+      lvlsn <- append(lvlsn, sum(covars[[av]] == lv, na.rm = TRUE))
+    }
+    legend("topright", legend = paste0(lvls, " (",lvlsn,")"),
+           fill = colpal, cex = 0.75)
     mtext(subtitle, side = 3, line = 0)
   }
 
